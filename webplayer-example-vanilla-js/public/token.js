c851107345f16cf12d883790b46db225cf5fc0cf
Hayden Bleasel <hello@haydenbleasel.com>
2023-01-06 12:04:30 -0500

    Update and rename token.sample.js to token.js

---
+++ webplayer-example-vanilla-js/public/token.js
@@ -0,0 +1,5 @@
+// Add the API token below. This API token should be kept private,
+// however for this simple example we're using a public import
+// since we don't use Babel or Typescript
+export const apiToken =
+  'Your Corellium API Access Token';
--- webplayer-example-vanilla-js/public/token.sample.js
+++
@@ -1,5 +0,0 @@
-// Rename this file to token.js and add the API token below
-// this API token should be kept private, however for this simple example we're
-// using a public import since we don't use Babel or Typescript
-export const apiToken =
-  'Your Corellium API Access Token';
