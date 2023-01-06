985b8e15b1fb4e7deea7c64f4be233859f59c8f7
Hayden Bleasel <hello@haydenbleasel.com>
2023-01-06 12:02:34 -0500

    Update server.js

--- webplayer-example-vanilla-js/server.js
+++ webplayer-example-vanilla-js/server.js
@@ -32,7 +32,7 @@ app.post('/api/auth', jsonParser, async (req, res) => {
       },
       body: JSON.stringify({
         projectId: req.body.projectId,
-        instancedId: req.body.deviceId,
+        instanceId: req.body.deviceId,
         expiresIn: 18000,
         features: req.body.features,
       }),
